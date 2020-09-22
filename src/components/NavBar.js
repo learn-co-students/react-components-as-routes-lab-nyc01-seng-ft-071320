1c7adfee0bf7945dc9a1664ec088a03409c91897
humorous-storage-7521 <rmwerner@umich.edu>
2020-09-22 16:06:09 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Movies from "./Movies"
+import Directors from "./Directors"
+import Actors from "./Actors"
 
-const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+
+const NavBar = () => (
+  <div className="navbar">
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/movies">Movies</NavLink>
+    <NavLink to="/directors">Directors</NavLink>
+    <NavLink to="/actors">Actors</NavLink>
+  </div>
+)
 
 export default NavBar;
