38bfcda20e922739ff339b42d2233ea5f185861e
junko911 <junkotahara911@gmail.com>
2020-09-22 18:50:08 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { actors } from '../data';
+// import Movies from './Movies';
 
 const Actors = () => {
+
+  const renderActors = () => {
+    return actors.map(actor => {
+
+      const movies = () => actor.movies.map(movie => <li>{movie}</li>)
+      
+      return (
+        <div>
+          <h2>Name: {actor.name}</h2>
+          <p>Movies:</p>
+          <ul>{movies()}</ul>
+        </div>
+      )
+    })
+  }
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
