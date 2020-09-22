8dd06e1f0cdec608867deee5bc6f62a981eaf434
Mimioj101 <mimioj@gmail.com>
2020-09-22 19:28:11 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { actors } from '../data';
 
+
+const renderActor = () => {
+  return actors.map(act => 
+    <div>
+      <h4>{act.name}</h4>
+      <p>{act.movies}</p>
+    </div>)
+}
+
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActor()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderMovie = () => {
+  return movies.map(mov => 
+    <div>
+      <h4>{mov.title}</h4>
+      <p>Runtime: {mov.time}</p>
+      <p>Genres: {mov.genres}</p>
+    </div>)
+  }
+
+
 const Movies = () => {
+  // console.log(movies)
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovie()}
     </div>
   );
 };
