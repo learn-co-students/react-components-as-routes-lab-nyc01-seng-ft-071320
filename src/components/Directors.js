ddb85dc29326a7e0ba77616b6c9c61117ad32979
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-23 00:05:18 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,16 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(el =>
+        <div>
+          <p>{el.name}</p>
+          <ul>
+            {el.movies.map(el => 
+            <li>{el}</li>)}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      { movies.map(el => 
+        <div>
+          <p>Title: {el.title}</p>
+          <p>Time: {el.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {el.genres.map(el =>
+            <li>{el}</li>
+            )}
+          </ul>
+        </div> 
+      )} 
     </div>
   );
 };
